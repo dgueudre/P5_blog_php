a4ff01f36b5bcae46b24822f54526cf167f07e2f
Naime Medjek <naimemedjek@gmail.com>
2021-10-26 09:26:20 +0200

    requete préparée nominative

--- req.php
+++ req.php
@@ -10,7 +10,12 @@ catch(Exception $e)
 }
 
 $req = $bdd->prepare('SELECT nom, prix FROM jeux_video WHERE possesseur = ? AND prix <= ? ORDER BY prix');
-$req->execute(array($_GET['possesseur'],$_GET['prix_max'])); 
+$req->execute(array($_GET['possesseur'],$_GET['prix_max']));
+
+// Autre manière de faire :
+
+// $req = $bdd->prepare('SELECT nom, prix FROM jeux_video WHERE possesseur = :possesseur AND prix <= :prixmax ORDER BY prix');
+// $req->execute(array('possesseur' => $_GET['possesseur'], 'prixmax' => $_GET['prix_max']));;
 
 while($donnees=$req->fetch()) { ?>
 <ul><li> <?php echo $donnees['nom']  . ' coute ' . $donnees['prix'] . ' euros'; ?></li> </ul>
