4351f25174203dc90897768d9147cb07c3c44bce
Naime Medjek <naimemedjek@gmail.com>
2021-10-28 16:48:27 +0200

    base blog mvc & poo

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,28 +1,36 @@
 <?php
 
-require('model/frontend.php');
+// Chargement des classes
+require_once('model/PostManager.php');
+require_once('model/CommentManager.php');
 
 function listPosts()
 {
-    $posts = getPosts();
+    $postManager = new \OpenClassrooms\Blog\Model\PostManager();
+    $posts = $postManager->getPosts();
 
-    require('view/listPostsView.php');
+    require('view/frontend/listPostsView.php');
 }
 
 function post()
 {
-    $post = getPost($_GET['id']);
-    $comments = getComments($_GET['id']);
+    $postManager = new \OpenClassrooms\Blog\Model\PostManager();
+    $commentManager = new \OpenClassrooms\Blog\Model\CommentManager();
 
-    require('view/postView.php');
+    $post = $postManager->getPost($_GET['id']);
+    $comments = $commentManager->getComments($_GET['id']);
+
+    require('view/frontend/postView.php');
 }
 
 function addComment($postId, $author, $comment)
 {
-    $affectedLines = postComment($postId, $author, $comment);
+    $commentManager = new \OpenClassrooms\Blog\Model\CommentManager();
+
+    $affectedLines = $commentManager->postComment($postId, $author, $comment);
 
     if ($affectedLines === false) {
-        die('Impossible d\'ajouter le commentaire !');
+        throw new Exception('Impossible d\'ajouter le commentaire !');
     }
     else {
         header('Location: index.php?action=post&id=' . $postId);
